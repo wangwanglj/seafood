27571ede1552fb6e2a9d0574726b9367980d1307
1175572685@qq.com <1175572685@qq.com>
2019-04-21 23:28:30 +0800

    商品信息

---
+++ server/src/main/java/com/weixin/dao/GoodTypeDao.java
@@ -0,0 +1,8 @@
+package com.weixin.dao;
+
+import com.weixin.beans.GoodType;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface  GoodTypeDao extends  JpaRepository<GoodType,Integer>{
+
+}
