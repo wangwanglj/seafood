f80d5bb3dbf72339a78231054aa42c8eda391be6
wangwanglj <1175572685@qq.com>
2019-05-05 11:47:32 +0800

    忽略文件

--- server/src/main/java/com/weixin/dao/ItemDao.java
+++
@@ -1,7 +0,0 @@
-package com.weixin.dao;
-
-import com.weixin.beans.Item;
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface ItemDao extends JpaRepository<Item,Integer> {
-}
